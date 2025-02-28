64912a24306d5c82ff4592b27062ab7100e06069
avadaliDevOps <adityavittal92@gmail.com>
2025-02-28 19:52:38 +0530

    update the code

---
+++ env-prod/main.tfvars
@@ -0,0 +1,26 @@
+instance = {
+  mongodb = {
+	ami_id                 = "ami-09c813fb71547fc4f"
+	instance_type          = "t3.small"
+	zone_id                = "Z02980273R2SC8CB35MNX"
+	vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
+
+
+  }
+  catalogue = {
+	ami_id                 = "ami-09c813fb71547fc4f"
+	instance_type          = "t3.small"
+	zone_id                = "Z02980273R2SC8CB35MNX"
+	vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
+
+
+  },
+  frontend = {
+	ami_id                 = "ami-09c813fb71547fc4f"
+	instance_type          = "t3.small"
+	zone_id                = "Z02980273R2SC8CB35MNX"
+	vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
+  }
+
+}
+env = prod
--- variables.tf
+++ variables.tf
@@ -1,31 +1,3 @@
-variable "instance" {
-  default = {
-    mongodb = {
-      ami_id        = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-      zone_id       = "Z02980273R2SC8CB35MNX"
-      vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
+variable "instance" {}
 
-
-    }
-    catalogue = {
-      ami_id        = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-      zone_id       = "Z02980273R2SC8CB35MNX"
-      vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
-
-
-    },
-    frontend = {
-      ami_id        = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-      zone_id       = "Z02980273R2SC8CB35MNX"
-      vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
-
-
-    }
-  }
-}
-variable "env" {
-  default = "dev"
-}
+variable "env" {}
