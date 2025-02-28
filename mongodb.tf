a108424df19410c338edd3ee2e2b5d9f9c267ec5
avadaliDevOps <adityavittal92@gmail.com>
2025-02-28 15:40:27 +0530

    update the code

---
+++ mongodb.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    ad = {
+      source  = "hashicorp/ad"
+      version = "0.5.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+}
+# ec2 instance create
+
+resource "aws_instance" "mongodb" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
+  tags = {
+    Name = "mongodb"
+      }
+
+}
+
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z02980273R2SC8CB35MNX"
+  name    = "mongodb-dev"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "null_resource" "mongodb" {
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      password = "DevOps321"
+      host = aws_instance.mongodb.private_ip
+    }
+    inline = [
+      "sudo pip3 install ansible",
+      "ansible-pull -i localhost, -U https://github.com/vadalibhavya/roboshop-ansible roboshop.yml -e env=dev -e role_name=mongodb -e component_name=mongodb"
+
+    ]
+  }
+
+}
