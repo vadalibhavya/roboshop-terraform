5a47ab61e83472f4cf57b0caa866f3d969368efc
avadaliDevOps <adityavittal92@gmail.com>
2025-02-28 13:07:11 +0530

    update frontend.tf

--- frontend.tf
+++ frontend.tf
@@ -3,6 +3,7 @@
 resource "aws_instance" "frontend" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
   tags = {
     Name = "frontend"
       }
