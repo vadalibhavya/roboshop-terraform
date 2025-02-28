54f12b14d94fccc95c25c1bbc39992f0c71efc6a
avadaliDevOps <adityavittal92@gmail.com>
2025-02-28 18:05:46 +0530

    update the code

--- variables.tf
+++ variables.tf
@@ -3,27 +3,29 @@ variable "instance" {
     mongodb = {
       ami_id = "ami-09c813fb71547fc4f"
       instance_type = "t3.small"
-      vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
-      zone_id = "Z02980273R2SC8CB35MNX"
+
 
     }
     catalogue = {
       ami_id = "ami-09c813fb71547fc4f"
       instance_type = "t3.small"
-      vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
-      zone_id = "Z02980273R2SC8CB35MNX"
+
 
     },
     frontend = {
       ami_id = "ami-09c813fb71547fc4f"
       instance_type = "t3.small"
-      vpc_security_group_ids = ["sg-0d39c28f1d7e34d30"]
-      zone_id = "Z02980273R2SC8CB35MNX"
+
 
     }
   }
 }
-
+variable "zone_id" {
+  default = "Z02980273R2SC8CB35MNX"
+}
+variable "vpc_security_group_ids" {
+  default = ["sg-0d39c28f1d7e34d30"]
+}
 variable "env" {
   default = "dev"
 }
