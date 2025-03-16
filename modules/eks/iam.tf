2524346840ed81ccd26f88dc6958960d200efb6c
aditya <adityavittal92@gmail.com>
2025-03-16 22:41:27 +0530

    test

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -1,22 +1,21 @@
 resource "aws_iam_role" "cluster" {
-  name = "${var.env}-eks-cluster-role"
-  assume_role_policy = jsondecode({
-    version = "2012-10-17"
-    statement = [
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-        action = [
+        Action = [
           "sts:AssumeRole",
           "sts:TagSession"
         ]
-        principal = {
-          service = "eks.amazonaws.com"
+        Principal = {
+          Service = "eks.amazonaws.com"
         }
-        effect = "Allow"
-
+        Effect = "Allow"
       }
     ]
   })
 }
+
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
